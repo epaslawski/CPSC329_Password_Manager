0e5907f4577003b5b3f27830360dbb90a224e62c
Erin Paslawski <erin.paslawski@gmail.com>
2021-06-14 21:12:38 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ height_setting = 677
 
 set_main_window_size(540, 720)
 set_global_font_scale(1.25)
-set_theme("Gold")
+set_theme("White")
 set_style_window_padding(30, 30)
 
 
@@ -19,10 +19,56 @@ def check_login_callback(sender, data):
     print("Checking password")
     if get_value("Password") == 'password':
         print("Passwords is correct.")
+        window_close("Login", data)
+        open_main
 
 
+def open_main():
+    if does_item_exist("Main Page"):
+        show_item("Main Page")
+    else:
+        add_window("Main Page")
 
-def on_window_close(sender, data):
+
+def add_password_callback(sender, data):
+    window_close("Main Page")
+
+    # add input box
+
+    # add button
+
+    # add back button
+
+    add_button("Main Page", callback=open_main)
+
+
+def view_passwords_callback(args):
+    pass
+
+
+def check_strength_callback(args):
+    pass
+
+
+with window("Main Page", width=width_setting, height=height_setting):
+    print("Welcome to the Password Manager.")
+    set_window_pos("Main Page", 0, 0)
+    add_drawing("logo", width=520, height=290)
+
+    # IF THE PREVIOUS LINE OF CODE TRIGGERS AN ERROR TRY
+    draw_image("logo", "Logo.png", [0, 40], [420, 260])
+
+    # add buttons for doing stuff
+    # add password
+    add_button("Add Password", callback=add_password_callback)
+
+    # view passwords
+    add_button("View Passwords", callback=view_passwords_callback)
+
+    # Check strength
+    add_button("Check password strength", callback=check_strength_callback)
+
+def window_close(sender, data):
     delete_item(sender)
     log_debug("window was deleted")
 
