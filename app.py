4cd40350e06bf59bce8173e1f6ecbd39cdb87ff2
Erin Paslawski <erin.paslawski@gmail.com>
2021-06-14 22:09:36 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,44 +2,44 @@
 from dearpygui.core import *
 from dearpygui.simple import *
 
+import functions
 
 width_setting = 520
 height_setting = 677
 
 set_main_window_size(540, 720)
 set_global_font_scale(1.25)
-set_theme("White")
+set_theme("Gold")
 set_style_window_padding(30, 30)
 
 
-def check_login_callback(sender, data):
+def check_login_callback(sender):
     # open text file
     # get master password
     # compare to input
     print("Checking password")
     if get_value("Password") == 'password':
         print("Passwords is correct.")
-        window_close("Login", data)
+        window_close("Login")
         open_main
 
 
-def open_main():
+def open_main(sender):
     if does_item_exist("Main Page"):
         show_item("Main Page")
     else:
         add_window("Main Page")
+    # close others to be safe
+    if does_item_exist("Login"):
+        window_close("Login")
 
 
-def add_password_callback(sender, data):
-    window_close("Main Page")
-
-    # add input box
-
-    # add button
-
-    # add back button
-
-    add_button("Main Page", callback=open_main)
+def add_password_callback(sender):
+    hide_item("Main Page")
+    if does_item_exist("Add Password"):
+        show_item("Add Password")
+    else:
+        add_window("Add Password")
 
 
 def view_passwords_callback(args):
@@ -50,9 +50,37 @@ def check_strength_callback(args):
     pass
 
 
-with window("Main Page", width=width_setting, height=height_setting):
+def add_password(sender, data):
+    # code for adding the new password to the database, including encryption
+    print("Adding Password")
+    functions.add_password(get_value("Username"), get_value("New Password"))
+    # window_close("Add Password")
+    hide_item("Add Password")
+    open_main
+
+
+with window("Add Password", width=width_setting, height=height_setting, no_collapse=True, no_resize=True, no_close=True,
+            no_move=True, no_background=False):
+    print("Add a new password.")
+    set_window_pos("Add Password", 0, 0)
+
+    add_text("Enter the credentials to be added:")
+    add_spacing(count=5)
+
+    # collect password input
+    add_input_text("Username", width=250, default_value="Username")
+    add_input_text("New Password", width=250, default_value="New password")
+    add_button("Add", callback=add_password)
+
+    add_spacing(count=30)
+
+    add_button("Return", callback=open_main)
+
+
+with window("Main Page", width=width_setting, height=height_setting, y_pos=0, x_pos=0, no_collapse=True, no_resize=True, no_close=True,
+            no_move=True, no_background=False):
     print("Welcome to the Password Manager.")
-    set_window_pos("Main Page", 0, 0)
+
     add_drawing("logo", width=520, height=290)
 
     # IF THE PREVIOUS LINE OF CODE TRIGGERS AN ERROR TRY
@@ -60,7 +88,7 @@ with window("Main Page", width=width_setting, height=height_setting):
 
     # add buttons for doing stuff
     # add password
-    add_button("Add Password", callback=add_password_callback)
+    add_button("Add a Password", callback=add_password_callback)
 
     # view passwords
     add_button("View Passwords", callback=view_passwords_callback)
@@ -68,23 +96,26 @@ with window("Main Page", width=width_setting, height=height_setting):
     # Check strength
     add_button("Check password strength", callback=check_strength_callback)
 
-def window_close(sender, data):
+
+def window_close(sender):
     delete_item(sender)
     log_debug("window was deleted")
 
 
-with window("Login", width=width_setting, height=height_setting):
+with window("Login", width=width_setting, height=height_setting, no_collapse=True, no_resize=True, no_close=True,
+            no_move=True, no_background=False):
     print("Login to Password Manager.")
+    # hide the other windows and wait for the master password
+
     set_window_pos("Login", 0, 0)
     # add_drawing("logo", width=520, height=290)
     add_text("Enter your master password: ")
     add_spacing(count=5)
 
     # collect password input
-    add_input_text("Password", width=250, default_value="Add Password here.")
+    add_input_text("Password", width=250, default_value="password")
     add_same_line()  # add button beside input
     add_button("Enter", callback=check_login_callback)
-
 
 start_dearpygui()
 print("Goodbye!")
--- functions.py
+++ functions.py
@@ -1,1 +1,13 @@
 # for adding the hashing functions, password storage IO, etc
+
+def add_password(usrnam, pswrd):
+    pass
+    # TODO
+
+def encrtpy_pass(pswrd):
+    pass
+    # TODO
+
+def return_leet(pswrd):
+    pass
+    # TODO
