1d66b707d55b0d4772d00af4493ffe6e8d019732
RPyaanng <RyanPang12@gmail.com>
2021-06-15 09:02:16 -0600

    Added: Password Creation + Viewing
    Added Encryption and ability to add passwords and to view them.
    
    TODO:
    Delete & Edit passwords

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 # DearPyGUI Imports
 from dearpygui.core import *
 from dearpygui.simple import *
+<<<<<<< Updated upstream
+
+=======
+from smart_table import SmartTable
+import functions
+>>>>>>> Stashed changes
+
+cred_buf = []
+enc_key = b''
+tbl = None
 
 
 width_setting = 520
@@ -11,18 +21,144 @@ set_global_font_scale(1.25)
 set_theme("Gold")
 set_style_window_padding(30, 30)
 
+def confirm_mpw_callback(sender, data):
+    # compare inputs
+    print("Checking password")
+    if get_value("Master Password") == get_value("Re-enter Master Password"):
+        print("Passwords is correct.")
+        #initialize passwords.txt
+        functions.initPWFile(bytes(get_value("Master Password"), "utf-8"))
+        window_close("Register")
+        open_main()
+    else:
+        print("Passwords do not match!")
+        add_popup(popupparent="Re-enter Master Password", name="popup", show=True, width= 40, height=20)
+        add_text("Passwords do not match!")
+        add_button("Close", callback=close_popup("popup"))
 
 def check_login_callback(sender, data):
-    # open text file
     # get master password
     # compare to input
     print("Checking password")
-    if get_value("Password") == 'password':
+    if functions.check_mpw(get_value("Password")):
         print("Passwords is correct.")
+<<<<<<< Updated upstream
 
 
 
 def on_window_close(sender, data):
+=======
+        functions.get_list(get_value("Password"))
+        global enc_key, cred_buf, tbl
+        enc_key = functions.get_key()
+        cred_buf = functions.get_list(get_value("Password"))
+        window_close("Login")
+        open_main()
+        
+    else:
+        print("Wrong Password!")
+        add_popup(popupparent="Password", name="popup", show=True, width= 40, height=20)
+        add_text("Wrong Password")
+        add_button("Close", callback=close_popup("popup"))
+
+
+def open_main():
+    if does_item_exist("Main Page"):
+        show_item("Main Page")
+    else:
+        add_window("Main Page")
+        
+    # close others to be safe
+    if does_item_exist("Login"):
+        window_close("Login")
+    if does_item_exist("Register"):
+        window_close("Register")
+    if does_item_exist("Add Password"):
+        hide_item("Add Password")
+    populate_table()
+
+
+def open_register(sender, data):
+    if does_item_exist("Login"):
+        window_close("Login")
+    add_window("Register")
+
+
+def add_password_callback(sender, data):
+    hide_item("Main Page")
+    if does_item_exist("Add Password"):
+        show_item("Add Password")
+    else:
+        add_window("Add Password")
+
+def populate_table():
+    global cred_buf, tbl
+    #add_table(parent="Main Page", name="table", headers=["Username","Password","Website"])
+    print(cred_buf)
+    for cred in cred_buf:
+        add_row("table",[cred[1],cred[2],cred[0]])
+        
+
+def view_passwords_callback(sender, data):
+    pass
+
+
+def check_strength_callback(sender, data):
+    pass
+
+
+def add_password(row=[]):
+    # code for adding the new password to the database, including encryption
+    global cred_buf
+    cred_buf.append(row)
+    print("Adding Password")
+    functions.add_password(row, enc_key)
+    add_row("table",[row[1],row[2],row[0]])
+    # window_close("Add Password")
+    hide_item("Add Password")
+    show_item("Main Page")
+
+
+with window("Add Password", width=width_setting, height=height_setting, no_collapse=True, no_resize=True, no_close=True,
+            no_move=True, no_background=False):
+    print("Add a new password.")
+    set_window_pos("Add Password", 0, 0)
+
+    add_text("Enter the credentials to be added:")
+    add_spacing(count=5)
+
+    # collect password input
+    add_input_text("Account", width=250, default_value="")
+    add_input_text("Username", width=250, default_value="")
+    add_input_text("New Password", width=250, default_value="")
+    add_button("Add", callback=lambda x,y:add_password(row=[get_value("Account"), get_value("Username"), get_value("New Password")]))
+
+    add_spacing(count=30)
+
+    add_button("Cancel", callback=open_main)
+
+
+with window("Main Page", width=width_setting, height=height_setting, y_pos=0, x_pos=0, no_collapse=True, no_resize=True, no_close=True,
+            no_move=True, no_background=False):
+    # add buttons for doing stuff
+    # add password
+    add_button("Add a Password", callback=add_password_callback)
+    add_same_line()  # add button beside input
+    # view passwords
+    add_button("View Passwords", callback=view_passwords_callback)
+    print("Welcome to the Password Manager.")
+
+    add_table(parent="Main Page", name="table", headers=["Username","Password","Website"])
+
+    # Add the logo
+    draw_image("logo", "Logo.png", [0, 40], [420, 260])
+
+    # Check strength
+    add_button("Check password strength", callback=check_strength_callback)
+
+
+def window_close(sender):
+>>>>>>> Stashed changes
     delete_item(sender)
     log_debug("window was deleted")
 
@@ -40,5 +176,31 @@ with window("Login", width=width_setting, height=height_setting):
     add_button("Enter", callback=check_login_callback)
 
 
+<<<<<<< Updated upstream
+=======
+with window("Register", width=width_setting, height=height_setting, no_collapse=True, no_resize=True, no_close=True,
+            no_move=True, no_background=False):
+    print("Register Password")
+    
+    # hide the other windows and wait for the master password
+
+    set_window_pos("Register", 0, 0)
+    # add_drawing("logo", width=520, height=290)
+    add_text("Create your master password: ")
+    add_spacing(count=5)
+
+    # collect password input
+    add_input_text("Master Password", width=250)
+    add_input_text("Re-enter Master Password", width=250)
+    add_button("Confirm", callback=confirm_mpw_callback)
+    try:
+        f = open("passwords.txt","x")
+    except:
+        print("passwords.txt exists")
+        window_close("Register")
+
+
+>>>>>>> Stashed changes
 start_dearpygui()
+show_debug()
 print("Goodbye!")
--- functions.py
+++ functions.py
@@ -1,1 +1,56 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
 # for adding the hashing functions, password storage IO, etc
+<<<<<<< Updated upstream
+=======
+def add_password(row, enc):
+    print(enc)
+    f = Fernet(enc)
+    #append line
+    file = open("passwords.txt","a")
+    file.write(f.encrypt(bytes(row[0] + "," + row[1] + "," + row[2], encoding='utf-8')).decode("utf-8")+"\n")
+
+def return_leet(pswrd):
+    pass
+    # TODO
+
+def writeToFile():
+    fw = open("passwords.txt", "w")
+    pass
+    # TODO
+
+def initPWFile(mpw):
+    file = open("passwords.txt","w")
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(mpw))
+    f = Fernet(key)
+    token = f.encrypt(b"Open Sesame!")
+    file.writelines([key.decode("utf-8") + "\n", token.decode("utf-8") + "\n"])
+
+def check_mpw(mpw):
+    file = open("passwords.txt","r").readlines()
+    f = Fernet(file[0].encode('utf8', 'strict'))
+    return f.decrypt(file[1].encode('utf8', 'strict')) == b'Open Sesame!'
+
+def get_list(mpw):
+    res = []
+    file = open("passwords.txt","r").readlines()
+    f = Fernet(file[0].encode('utf8', 'strict'))
+    for cred in file[2:]:
+        res.append(f.decrypt(cred.encode('utf8', 'strict')).decode('utf-8').split(","))
+    return res
+
+def get_key():
+    file = open("passwords.txt","r").readlines()
+    return file[0].encode('utf8', 'strict')
+>>>>>>> Stashed changes
