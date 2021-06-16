f9c817a0aeed2a665c669b9143c2a02bf585fdcd
moboss14 <85786872+moboss14@users.noreply.github.com>
2021-06-16 21:23:58 +0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -20,9 +20,9 @@ def cred_buffer_to_file(cred_buffer):
     #Take first 2 lines from password file
     byte_buffer = open("passwords.txt","r").read().splitlines()[:2]
     for cred in cred_buffer:
-        byte_buffer.append(f.encrypt(bytes(cred[0] + "," + cred[1] + "," + cred[2], encoding='UTF-8')).decode("UTF-8") + "\n")
+    byte_buffer.append(f.encrypt(bytes(cred[0] + "," + cred[1] + "," + cred[2], encoding='UTF-8')).decode("UTF-8") + "\n")
     with open("passwords.txt", "w") as file:
-        file.writelines(byte_buffer)
+    file.writelines(byte_buffer)
 
 # for adding the hashing functions, password storage IO, etc
 def enc_to_file(index, row):
