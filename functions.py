08f32ede4405ac466984dc7d66e75a1e4119fb8c
Erin Paslawski <erin.paslawski@gmail.com>
2021-06-16 11:08:52 -0600

    Add files via upload
    adding new function for l33t encoding and password strength

--- functions.py
+++ functions.py
@@ -8,38 +8,55 @@ Authors: Erin Paslawski, Ryan Pang"""
 import base64
 import os
 import re
-
+import sys
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-l33t_dict = {
-    'a': '@',
-    'I': '1',
-    'Z': '2',
-    'e': '3',
-    'A': '4',
-    's': '5',
-    'S': '$',
-    'O': '0',
-    'i': '!',
-    '#': 'H',
-    'x': '*',
-    'G': '6',
-    'T': '7',
-    'B': '8',
-    'C': '(',
-    'c': '<'
-}
+lock :Fernet
 
+def cred_buffer_to_file(cred_buffer):
+    global lock
+    f = lock
+    #Take first 2 lines from password file
+    byte_buffer = open("passwords.txt","r").read().splitlines()[:2]
+    for cred in cred_buffer:
+        byte_buffer.append(f.encrypt(bytes(cred[0] + "," + cred[1] + "," + cred[2], encoding='UTF-8')).decode("UTF-8") + "\n")
+    with open("passwords.txt", "w") as file:
+        file.writelines(byte_buffer)
 
 # for adding the hashing functions, password storage IO, etc
-def add_password(row, enc):
-    print(enc)
-    f = Fernet(enc)
-    # append line
-    file = open("passwords.txt", "a")
-    file.write(f.encrypt(bytes(row[0] + "," + row[1] + "," + row[2], encoding='utf-8')).decode("utf-8") + "\n")
+def enc_to_file(index, row):
+    # retrieve the locking mechanism
+    global lock
+    f = lock
+    #append line
+    bytephrase = f.encrypt(bytes(row[0] + "," + row[1] + "," + row[2], encoding='UTF-8')).decode("UTF-8")
+    with open("passwords.txt", "a") as file:
+        file.write(bytephrase + "\n")
+
+
+def check_strength(pswrd):
+    if pswrd is None:
+        return "Weak"
+    pswrd_tally=0
+    if len(pswrd) > 9:
+        pswrd_tally = pswrd_tally + 1
+    if re.search(r'\d', pswrd) is not None:
+        pswrd_tally = pswrd_tally + 1
+    if re.search(r'[A-Z]', pswrd) is not None:
+        pswrd_tally = pswrd_tally + 1
+    if re.search(r'[a-z]', pswrd) is not None:
+        pswrd_tally = pswrd_tally + 1
+    if re.search(r'\W', pswrd) is not None:
+        pswrd_tally = pswrd_tally + 1
+    print(pswrd)
+    if pswrd_tally > 4:
+        return "Strong"
+    elif pswrd_tally > 2:
+        return "Medium"
+    else:
+        return "Weak"
 
 
 def return_leet(pswrd):
@@ -85,66 +102,52 @@ def return_leet(pswrd):
         print(pswrd)
         return rrtn_pswrd
 
-
-def writeToFile():
+def write_to_file():
     fw = open("passwords.txt", "w")
     pass
     # TODO
 
-
-def initPWFile(mpw):
-    file = open("passwords.txt", "w")
-    salt = os.urandom(16)
+def init_pw_file(mpw):
+    salt = base64.b64encode(os.urandom(16))
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
         salt=salt,
         iterations=100000,
     )
-    key = base64.urlsafe_b64encode(kdf.derive(mpw))
+    key = base64.urlsafe_b64encode(kdf.derive(bytes(mpw, 'UTF-8')))
     f = Fernet(key)
     token = f.encrypt(b"Open Sesame!")
-    file.writelines([key.decode("utf-8") + "\n", token.decode("utf-8") + "\n"])
-
+    with open("passwords.txt", "x") as file:
+        file.write(salt.decode("UTF-8") + "\n")
+        file.write(token.decode("UTF-8") + "\n")
 
 def check_mpw(mpw):
-    file = open("passwords.txt", "r").readlines()
-    f = Fernet(file[0].encode('utf8', 'strict'))
-    return f.decrypt(file[1].encode('utf8', 'strict')) == b'Open Sesame!'
+    file = open("passwords.txt","r").read().splitlines()
+    salt = file[0].encode(encoding='UTF-8')
+    token = file[1].encode(encoding='UTF-8')
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    global lock
+    key = base64.urlsafe_b64encode(kdf.derive(bytes(mpw, 'UTF-8')))
+    f = Fernet(key)
+    lock = f # store the locking mechanism
+    return f.decrypt(token) == b'Open Sesame!'
 
-
-def get_list(mpw):
+def get_list():
+    # retrieve the locking mechanism
+    global lock
     res = []
-    file = open("passwords.txt", "r").readlines()
-    f = Fernet(file[0].encode('utf8', 'strict'))
+
+    #open  the save file
+    file = open("passwords.txt","r").read().splitlines()
+    
+    f = lock
     for cred in file[2:]:
-        res.append(f.decrypt(cred.encode('utf8', 'strict')).decode('utf-8').split(","))
+        res.append(f.decrypt(cred.encode('utf8', 'strict')).decode('UTF-8').split(","))
     return res
 
-
-def get_key():
-    file = open("passwords.txt", "r").readlines()
-    return file[0].encode('utf8', 'strict')
-
-
-def check_strength(pswrd):
-    if pswrd is None:
-        return "Weak"
-    pswrd_tally=0
-    if len(pswrd) > 9:
-        pswrd_tally = pswrd_tally + 1
-    if re.search(r'\d', pswrd) is not None:
-        pswrd_tally = pswrd_tally + 1
-    if re.search(r'[A-Z]', pswrd) is not None:
-        pswrd_tally = pswrd_tally + 1
-    if re.search(r'[a-z]', pswrd) is not None:
-        pswrd_tally = pswrd_tally + 1
-    if re.search(r'\W', pswrd) is not None:
-        pswrd_tally = pswrd_tally + 1
-    print(pswrd)
-    if pswrd_tally > 4:
-        return "Strong"
-    elif pswrd_tally > 2:
-        return "Medium"
-    else:
-        return "Weak"
